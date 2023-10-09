37d48c6750eac5e4929518b87b614e3fb863d358
DmitryRatnsk <styx1man2@gmail.com>
2023-10-09 14:05:51 +0300

    start creating dashboard with different widgets

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -2,11 +2,21 @@
 
 namespace App\Filament\Pages;
 
+use Filament\Panel;
+
 class Dashboard extends \Filament\Pages\Dashboard
 {
 
     protected static ?string $navigationLabel = 'Панель приборов';
 
     protected static ?string $title = 'Панель приборов';
-    // ...
+
+    protected int | string | array $columnSpan = 'full';
+
+    public function panel(Panel $panel): Panel
+    {
+        return $panel
+            // ...
+            ->widgets([]);
+    }
 }
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Pages\Auth\Login;
+use App\Filament\Pages\Auth\Register;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -21,30 +23,24 @@ use Illuminate\View\Middleware\ShareErrorsFromSession;
 class AdminPanelProvider extends PanelProvider
 {
 
-    protected static ?string $navigationLabel = 'Custom Navigation Label';
-
-    protected static ?string $title = 'Finance dashboard';
-
-
     public function panel(Panel $panel): Panel
     {
         return $panel
             ->default()
             ->id('admin')
             ->path('admin')
-            ->login()
+            ->login(Login::class)
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->registration(Register::class)
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
                 // Pages\Dashboard::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
-            ->widgets([
-                Widgets\AccountWidget::class,
-            ])
+            ->widgets([])
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
