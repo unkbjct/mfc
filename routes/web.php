d125ab99a50d9cd28a3f27f5518ce628bb232bca
DmitryRatnsk <styx1man2@gmail.com>
2023-10-11 23:01:06 +0300

    add readme

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Livewire\Livewire;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin');
 });
+
 
